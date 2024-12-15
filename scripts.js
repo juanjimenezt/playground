4dc78e79b1fe45235fa49f378a8dc9f0f33e8098
juanjimenezt <juanjose.jt@outlook.com>
2024-12-15 18:43:35 -0500

    Ultimos cambios TS2

--- scripts.js
+++ scripts.js
@@ -20,3 +20,20 @@ function openCity(cityName, elmnt, color) {
 
 // Activa la pestaña por defecto
 document.getElementById("defaultOpen").click();
+
+
+
+function contar() {
+    const texto = document.getElementById("texto").value; // Obtener el texto del input
+    const vocales = "aeiouáéíóúAEIOUÁÉÍÓÚ";
+    let contador = 0;
+
+    for (let letra of texto) {
+        if (vocales.includes(letra)) {
+            contador++;
+        }
+    }
+
+    // Mostrar el resultado en el HTML
+    document.getElementById("resultado").textContent = `El texto tiene ${contador} vocales.`;
+}
